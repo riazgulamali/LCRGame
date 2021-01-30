5c8ccb447937cd5c5f69c904b2f51fdf1dc5ce51
riazgulamali@yahoo.com <riazgulamali@yahoo.com>
2021-01-30 17:38:12 -0600

    Created the user interface for the game and loaded the game

--- LeftCenterRightDesktopUI/ViewModels/ShellViewModel.cs
+++ LeftCenterRightDesktopUI/ViewModels/ShellViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Caliburn.Micro;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace LeftCenterRightDesktopUI.ViewModels
 {
-    public class ShellViewModel
+    public class ShellViewModel : Conductor<object>
     {
+        private GameViewModel _game;
+        public ShellViewModel(GameViewModel game)
+        {
+            _game = game;
+            ActivateItem(_game);
+        }
     }
 }
