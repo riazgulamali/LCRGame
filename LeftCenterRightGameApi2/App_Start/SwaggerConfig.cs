bdcff2695cbab9ed62077841df9a6496a9039fa8
riazgulamali@yahoo.com <riazgulamali@yahoo.com>
2021-01-30 15:18:34 -0600

    Added the database project to save and reterieve data from sql server

--- LeftCenterRightGameApi2/App_Start/SwaggerConfig.cs
+++ LeftCenterRightGameApi2/App_Start/SwaggerConfig.cs
@@ -36,7 +36,7 @@ namespace LeftCenterRightGameApi2
 
                         // If you want the output Swagger docs to be indented properly, enable the "PrettyPrint" option.
                         //
-                        //c.PrettyPrint();
+                        c.PrettyPrint();
 
                         // If your API has multiple versions, use "MultipleApiVersions" instead of "SingleApiVersion".
                         // In this case, you must provide a lambda that tells Swashbuckle which actions should be
@@ -182,7 +182,7 @@ namespace LeftCenterRightGameApi2
                         // Use the "DocumentTitle" option to change the Document title.
                         // Very helpful when you have multiple Swagger pages open, to tell them apart.
                         //
-                        //c.DocumentTitle("My Swagger UI");
+                        c.DocumentTitle("Left Center Right Game");
 
                         // Use the "InjectStylesheet" option to enrich the UI with one or more additional CSS stylesheets.
                         // The file must be included in your project as an "Embedded Resource", and then the resource's
