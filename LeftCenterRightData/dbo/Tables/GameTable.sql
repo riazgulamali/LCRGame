c49854d35f1358784af021299204b06124da3df7
riazgulamali@yahoo.com <riazgulamali@yahoo.com>
2021-01-30 16:44:56 -0600

    Created Database Table required for the game

---
+++ LeftCenterRightData/dbo/Tables/GameTable.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[GameTable]
+(
+	[Id] INT NOT NULL IDENTITY,
+    [Userid] NCHAR(128) NOT NULL, 
+    [GameId] NCHAR(10) NOT NULL, 
+    [GameDate] DATETIME2 NOT NULL DEFAULT getutcdate(), 
+    [Dice1] INT NOT NULL, 
+    [Dice2] INT NOT NULL, 
+    [Dice3] INT NOT NULL, 
+    [Dice4] INT NOT NULL, 
+    [chipValue1] INT NULL,
+    [chipValue2] INT NULL,
+    [chipValue3] INT NULL,
+    [chipValue4] INT NULL, 
+    [CreateDate] DATETIME2 NOT NULL DEFAULT getutcdate()
+
+)
---
+++ LeftCenterRightData/dbo/Tables/UserInfo.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[UserInfo]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [AuthUserId] NVARCHAR(128) NOT NULL, 
+    [FirstName] NCHAR(50) NOT NULL,
+    [LastName] NCHAR(50) NOT NULL,
+    [EmailAddress   ] NCHAR(256) NOT NULL,
+    [CreateDate] DATETIME2 NOT NULL DEFAULT getutcdate()
+    
+)
