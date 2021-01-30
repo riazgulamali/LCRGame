0666d65c1d2ec9cf007caad774fd6299ce369bcb
riazgulamali@yahoo.com <riazgulamali@yahoo.com>
2021-01-30 14:54:10 -0600

    Added swagger to the api

---
+++ LeftCenterRightGameApi2/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace LeftCenterRightGameApi2.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET: Values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Value1","value2" };
+
+        }
+    }
+}
